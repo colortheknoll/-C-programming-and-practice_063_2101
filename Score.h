c5c5914ac69fa263886e6659c7d98ef0b475c2cd
colortheknoll <71335230+colortheknoll@users.noreply.github.com>
2021-05-24 13:42:47 +0900

    Add files via upload
    Block.cpp의 move_down, move_left,  move_right 함수 작성
    Score.cpp 및 score.h 작성
    Mainmenu.cpp 및 Mainmenu.h 작성

---
+++ Score.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Score
+{
+private:
+	int score;
+	int* speed;
+
+public:
+	Score(int *speed) {
+		score = 0;
+		this->speed = speed;
+	}
+
+	void score_up();
+	void print_score();
+};
